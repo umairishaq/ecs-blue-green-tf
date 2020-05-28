1535d00cc36754b0bde6354524d2e6a758876622
Umair Ishaq <umair29@gmail.com>
2020-05-28 15:46:03 -0400

    A new world.

--- src/index.py
+++ src/index.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Blueeeeeeeee skieeeeeeeeeees!!!!"
+    return "The new Greeeeeeeeeeeen!!!!"
 
 @app.route("/healthz")
 def healthz():
