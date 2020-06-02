74f9a08e9e254097143af9ea8b885d1088d4cedf
Umair Ishaq <umair29@gmail.com>
2020-06-02 10:42:35 -0400

    Green deployment.

--- src/index.py
+++ src/index.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "The is very Bluuuuuuuuuuuuuuuuuuuueeeeeeeee!!!!"
+    return "Hello Jeremy!!!!"
 
 @app.route("/healthz")
 def healthz():
